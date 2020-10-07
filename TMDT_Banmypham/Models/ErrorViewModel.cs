e8bb34484ce158a803d66cc117e1f3bbd6de9c40
NhatPeach <daoducnhat3010@gmail.com>
2020-10-07 15:39:37 +0700

    Add project files.

---
+++ TMDT_Banmypham/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TMDT_Banmypham.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
